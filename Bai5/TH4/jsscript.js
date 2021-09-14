67d1567ef2751c6759f6299ba604ac29a7c36275
hakhanhnamqb <hakhanhnam.qb@gmail.com>
2021-09-14 15:59:55 +0700

    Signed-off-by: hakhanhnamqb <hakhanhnam.qb@gmail.com>

---
+++ Bai5/TH4/jsscript.js
@@ -0,0 +1,8 @@
+function funct() {
+    let inputWidth = prompt('width = ');
+    let inputHeight = prompt('height = ');
+    let a = parseInt(inputWidth);
+    let b = parseInt(inputHeight);
+    let Area = a*b;
+    document.write('the area: ' + Area);
+}
