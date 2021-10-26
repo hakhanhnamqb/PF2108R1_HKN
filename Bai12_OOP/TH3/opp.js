ce4ae04d15a59f5d3c59091083ea728dd65c6602
hakhanhnamqb <hakhanhnam.qb@gmail.com>
2021-10-26 10:36:20 +0700

    Signed-off-by: hakhanhnamqb <hakhanhnam.qb@gmail.com>

--- Bai12_OOP/TH3/opp.js
+++ Bai12_OOP/TH3/opp.js
@@ -1,49 +1,77 @@
-class Battery{
-    constructor(){
+class Battery {
+    constructor() {
         this.energy = 0;
     }
-    setEnergy(energy){
+    setEnergy(energy) {
         this.energy = energy;
     };
-    getEnergy(){
+    getEnergy() {
         return this.energy;
     };
-    decreaseEnergy(){
-        if (this.energy>0){
+    decreaseEnergy() {
+        if (this.energy > 0) {
             this.energy--;
         }
     }
 };
 class FlashLamp {
-    constructor(){}
-    setBattery(battery){
+    constructor(battery) {
+        this.status = false;
+        this.battery = battery;
+    }
+    setBattery(battery) {
         this.battery = battery;
     };
-    getBatteryInfo(){
+    getBatteryInfo() {
         return this.battery.getEnergy();
     };
-    light(){
-        if (this.status){
+
+    light() {
+        if (this.status) {
             alert("Lighting");
         }
-        else{
+        else {
             alert("Not lighting");
         }
     };
-    turnOn (){
+    turnOn() {
         this.status = true;
+        this.battery.decreaseEnergy();
     };
-    turnOff (){
+    turnOff() {
         this.status = false;
+    };
+    getstt() {
+        return this.status;
     }
+
 };
 let battery = new Battery();
 battery.setEnergy(10);
 
-let flashlamp = new FlashLamp();
+let flashlamp = new FlashLamp(battery);
 flashlamp.setBattery(battery);
 
-document.write("Battery info: " + flashlamp.getBatteryInfo() + "<br/>");
+let turnon_bt = document.getElementById("turnon");
+let turnoff_bt = document.getElementById("turnoff");
+let stt_bt = document.getElementById("switch");
+let stt = flashlamp.getstt();
+turnon_bt.addEventListener('click', function () {
+    flashlamp.turnOn();
+    console.log("Battery info: " + flashlamp.getBatteryInfo() + "<br/>");
+    stt_bt.innerText = "Turn on";
+    console.log(stt);
+});
+turnoff_bt.addEventListener('click', function () {
+    flashlamp.turnOff();
+    console.log("Battery info: " + flashlamp.getBatteryInfo() + "<br/>");
+    stt_bt.innerText = "Turn off";
+    console.log(stt);
+});
+
+
+
+/* document.write("Battery info: " + flashlamp.getBatteryInfo() + "<br/>");
 flashlamp.light();
 
 document.write("Turn on<br/>");
@@ -53,4 +81,4 @@ document.write("Battery info: " + flashlamp.getBatteryInfo() + "<br/>");
 
 document.write("Turn off<br/>");
 flashlamp.turnOff();
-flashlamp.light();
+flashlamp.light(); */
