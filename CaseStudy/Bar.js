f7bc618d53af8189626456ed1ca9294d3d06a84e
hakhanhnamqb <hakhanhnam.qb@gmail.com>
2021-11-05 07:39:26 +0700

    Signed-off-by: hakhanhnamqb <hakhanhnam.qb@gmail.com>

--- CaseStudy/Bar.js
+++ CaseStudy/Bar.js
@@ -1,38 +1,38 @@
 class Bar {
     constructor(widthBar, xBar, yBar, velBar) {
-        this.widthBar = widthBar;
-        this.xBar = xBar;
-        this.yBar = yBar;
-        this.velBar = velBar;
+      this.widthBar = widthBar;
+      this.xBar = xBar;
+      this.yBar = yBar;
+      this.velBar = velBar;
     };
     //xác định phương pháp vẽ thanh Bar
     drawBar() {
-        ctx.beginPath();
-        ctx.fillStyle = 'rgb(155,0,0)';
-        ctx.fillRect(this.xBar, this.yBar, this.widthBar, 10);
-        ctx.fill();
-
+      ctx.beginPath();
+      ctx.fillStyle = 'rgb(155,0,0)';
+      ctx.fillRect(this.xBar, this.yBar, this.widthBar, 10);
+      ctx.fill();
+  
     };
     go_right() {
-        if (this.xBar + 100 < width) {
-            this.xBar += this.velBar;
-        }
+      if (this.xBar + this.widthBar < width) {
+        this.xBar += this.velBar;
+      }
     };
     go_left() {
-        if (this.xBar > 0) {
-            this.xBar -= this.velBar;
-        }
+      if (this.xBar > 0) {
+        this.xBar -= this.velBar;
+      }
     };
     clearRecLast() {
-        ctx.clearRect(this.xBar, this.yBar, this.widthBar, 10);
+      ctx.clearRect(this.xBar, this.yBar, this.widthBar, 10);
     };
     getxBar() {
-        return this.xBar;
+      return this.xBar;
     };
     getyBar() {
-        return this.yBar;
+      return this.yBar;
     };
     getWidthBar() {
-        return this.widthBar;
+      return this.widthBar;
     }
-}
+  }
--- CaseStudy/main.js
+++ CaseStudy/main.js
@@ -1,13 +1,17 @@
-const canvas = document.querySelector('canvas');
+// setup canvas
+const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-
+// setup size of canvas
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
-
+let point = 0;
+let play = 1;
+let alternate = true;
+let sound = document.getElementById("audio");
 // function to generate random number
 function random(min, max) {
-    const num = Math.floor(Math.random() * (max - min + 1)) + min;
-    return num;
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
 }
 // define array to store balls and populate it  //xác định mảng để lưu trữ các quả bóng và điền nó vào
 let balls = [];
@@ -15,10 +19,8 @@ let balls = [];
 while (balls.length < 1) {
   const size = random(10, 20);
   let ball = new Ball(
-    // ball position always drawn at least one ball width
-    // away from the adge of the canvas, to avoid drawing errors
-    random(0 + size, width- size),
-    random(0 + size*2,size*4),
+    random(0 + size, width - size),
+    random(0 + size * 2, size * 4),
     5,
     5,
     'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')',
@@ -28,42 +30,57 @@ while (balls.length < 1) {
 }
 
 // define Bar
-let bar = new Bar(200, (width / 2) - 100, height - 20, 30);
+let bar = new Bar(200, (width / 2) - 100, height - 20, 100);
 bar.drawBar();
 
 // define loop that keeps drawing the scene constantly   /định nghĩa vòng lặp giúp vẽ cảnh liên tục
-
+let disPoint = document.getElementById("point");
 function loop() {
-    let gameOver = false;
-    ctx.beginPath();
-    ctx.fillStyle = 'rgba(0,0,0,0.25)';
-    ctx.fillRect(0, 0, width, height);
+  let gameOver = false;
+  ctx.beginPath();
+  ctx.fillStyle = 'rgba(0,0,0,0.25)';
+  ctx.fillRect(0, 0, width, height);
 
-    for (let i = 0; i < balls.length; i++) {
-        balls[i].draw();
-        bar.drawBar();
-        balls[i].update();
-        if (balls[i].ballOver()) {
-            gameOver = true;
-        }
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    bar.drawBar();
+    balls[i].update();
+    if (balls[i].ballOver()) {
+      gameOver = true;
     }
-    if (!gameOver)
-        requestAnimationFrame(loop);
-    else {
-        console.log("game over");
+    balls[i].mark();
+  }
+  if (gameOver) {
+    clearInterval(play);
+    if (confirm("Congratulations, you got " + point + " points." + " Do you want to play again?") == true) {
+      gameOver = false;
+      location.reload();
     }
+  }
 }
 $(document).keydown(function (e) {
-    if ((e.keyCode || e.which) == 37) // left arrow
-    {
-        bar.clearRecLast();
-        bar.go_left();
-    }
-    else if ((e.keyCode || e.which) == 39)    // right arrow
-    {
-        bar.clearRecLast();
-        bar.go_right();
-    }
+  if ((e.keyCode || e.which) == 37) // left arrow
+  {
+    bar.clearRecLast();
+    bar.go_left();
+  }
+  else if ((e.keyCode || e.which) == 39)    // right arrow
+  {
+    bar.clearRecLast();
+    bar.go_right();
+  }
 });
-
-loop();
+function start() {
+  if (alternate) {
+    sound.play();
+    play = setInterval(loop, 15);
+    alternate = false;
+  }
+}
+function pause() {
+  if (!alternate) {
+    sound.pause();
+    clearInterval(play);
+    alternate = true;
+  }
+}
